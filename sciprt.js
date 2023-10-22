55a2e8035930d2d67f9df3da3fc3000a5b7f2c21
rahatpaudel <rahatpaudel@gmail.com>
2023-10-23 00:02:41 +0545

    First commit

---
+++ sciprt.js
@@ -0,0 +1,8 @@
+window.gtranslateSettings = {
+  default_language: "en",
+  native_language_names: true,
+  wrapper_selector: ".gtranslate_wrapper",
+  switcher_horizontal_position: "right",
+  switcher_vertical_position: "top",
+  float_switcher_open_direction: "bottom",
+};
